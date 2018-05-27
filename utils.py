cf90bc02c4cfb7678cb0894256d1705cf3b48c30
maruthibg <maruthibg@gmail.com>
2018-05-27 09:08:18 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,6 +5,10 @@ import re
 debug = config.debug
 
 
+class Packet(object):
+    pass
+
+
 def helper_imshow(name, image):
     cv2.imshow(name, image)
 
@@ -19,8 +23,17 @@ def helper_showwait(name, image):
         helper_imshow(name, image)
         helper_imwait()
 
+
 def is_letters(value):
     return bool(re.match('^[A-Z]+$', value))
 
+
 def is_digits(value):
     return bool(re.match('^[0-9]+$', value))
+
+
+def validate(value):
+    if len(value) == 9:
+        if is_letters(value[:3]):
+            if is_digits(value[3:]):
+                return value
